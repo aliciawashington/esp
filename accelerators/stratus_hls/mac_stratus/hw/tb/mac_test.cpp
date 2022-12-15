6c7424b76c1921fcb85def21c7bd71c376233b71
Alicia Washington <aw3405@columbia.edu>
2022-12-15 16:16:00 -0500

    Additional tb files for extra 2048 functions

---
+++ accelerators/stratus_hls/mac_stratus/hw/tb/mac_test.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2011-2021 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#include "mac_test.hpp"
---
+++ accelerators/stratus_hls/mac_stratus/hw/tb/mac_test.hpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2011-2021 Columbia University, System Level Design Group
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef TEST_MAC_H
+#define TEST_MAC_H
+
+
+
+
+#endif /* TEST_MAC_H */
